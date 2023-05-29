523fe8a17711d71629861613c7cb0737f41121fd
Dinethri Gunawardhana <dinethriupeksha200@gmail.com>
2023-05-29 15:08:15 +0530

    create pages

---
+++ workout_tracker/lib/models/exercise.dart
@@ -0,0 +1,15 @@
+class Exercise {
+  late final String name;
+  late final String weight;
+  late final String reps;
+  late final String sets;
+  late bool isCompleted;
+
+  Exercise({
+   required this.name,
+   required this.weight,
+   required this.reps,
+   required this.sets,
+   this.isCompleted = false,
+});
+}
---
+++ workout_tracker/lib/models/workout.dart
@@ -0,0 +1,10 @@
+import 'exercise.dart';
+
+class Workout {
+  late final String name;
+  late final List<Exercise> exercises;
+
+  Workout({required this.name, required this.exercises});
+
+
+}
