34aa44c2850008dba83608a4e1c51841b7274573
Dinethri Gunawardhana <dinethriupeksha200@gmail.com>
2023-06-02 13:12:21 +0530

    component create

---
+++ workout_tracker/lib/components/exercise_tile.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class ExerciseTile extends StatelessWidget {
+  final String exerciseName;
+  final String weight;
+  final String reps;
+  final String sets;
+  final bool isCompleted;
+  void Function(bool?)? onCheckBoxChanged;
+
+  ExerciseTile({
+    super.key,
+    required this.exerciseName,
+    required this.weight,
+    required this.reps,
+    required this.sets,
+    required this.isCompleted,
+    required this.onCheckBoxChanged,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.grey[200],
+      child: ListTile(
+        title: Text(exerciseName),
+        subtitle: Row(
+          children: [
+            //weight
+            Chip(label: Text("${weight}kg"),
+            ),
+
+            //reps
+            Chip(label: Text("$reps reps"),
+            ),
+
+            //sets
+            Chip(label: Text("$sets sets"),),
+          ],
+        ), trailing: Checkbox(value: isCompleted,
+      onChanged: (value)=> onCheckBoxChanged!(value),
+      ),
+      ),
+    );
+  }
+}
--- workout_tracker/lib/pages/workout_page.dart
+++ workout_tracker/lib/pages/workout_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:workout_tracker/components/exercise_tile.dart';
 import 'package:workout_tracker/data/workout_data.dart';
 
 class WorkoutPage extends StatefulWidget {
@@ -11,6 +12,11 @@ class WorkoutPage extends StatefulWidget {
 }
 
 class _WorkoutPageState extends State<WorkoutPage> {
+  void onCheckBoxChanged(String workoutName, String exerciseName) {
+    Provider.of<WorkoutData>(context, listen: false)
+        .checkOffExercise(workoutName, exerciseName);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Consumer<WorkoutData>(
@@ -19,13 +25,39 @@ class _WorkoutPageState extends State<WorkoutPage> {
           title: Text(widget.workoutName),
         ),
         body: ListView.builder(
-            itemCount: value.numberOfExercisesInWorkout(widget.workoutName),
-            itemBuilder: (context, index) => ListTile(
-                  title: Text(value
-                      .getRelevantWorkout(widget.workoutName)
-                      .exercises[index]
-                      .name),
-                )),
+          itemCount: value.numberOfExercisesInWorkout(widget.workoutName),
+          itemBuilder: (context, index) => ExerciseTile(
+            exerciseName: value
+                .getRelevantWorkout(widget.workoutName)
+                .exercises[index]
+                .name,
+            weight: value
+                .getRelevantWorkout(widget.workoutName)
+                .exercises[index]
+                .weight,
+            reps: value
+                .getRelevantWorkout(widget.workoutName)
+                .exercises[index]
+                .reps,
+            sets: value
+                .getRelevantWorkout(widget.workoutName)
+                .exercises[index]
+                .sets,
+            isCompleted: value
+                .getRelevantWorkout(widget.workoutName)
+                .exercises[index]
+                .isCompleted,
+            onCheckBoxChanged: (val)
+            {onCheckBoxChanged(
+              widget.workoutName,
+              value
+                  .getRelevantWorkout(widget.workoutName)
+                  .exercises[index]
+                  .name,
+            );
+            }
+          ),
+        ),
       ),
     );
   }
