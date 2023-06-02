2d2ec068a7a9a860e60f6d03408e4428162135d2
Dinethri Gunawardhana <dinethriupeksha200@gmail.com>
2023-06-02 10:21:39 +0530

    Update workout_page.dart

--- workout_tracker/lib/pages/workout_page.dart
+++ workout_tracker/lib/pages/workout_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:workout_tracker/data/workout_data.dart';
 
 class WorkoutPage extends StatefulWidget {
   final String workoutName;
@@ -11,9 +13,19 @@ class WorkoutPage extends StatefulWidget {
 class _WorkoutPageState extends State<WorkoutPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.workoutName),
+    return Consumer<WorkoutData>(
+      builder: (context, value, child) => Scaffold(
+        appBar: AppBar(
+          title: Text(widget.workoutName),
+        ),
+        body: ListView.builder(
+            itemCount: value.numberOfExercisesInWorkout(widget.workoutName),
+            itemBuilder: (context, index) => ListTile(
+                  title: Text(value
+                      .getRelevantWorkout(widget.workoutName)
+                      .exercises[index]
+                      .name),
+                )),
       ),
     );
   }
