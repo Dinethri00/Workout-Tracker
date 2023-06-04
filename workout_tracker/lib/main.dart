ff9312e48060b5d5f19b0977caf0bbd54f79ed4b
Dinethri Gunawardhana <dinethriupeksha200@gmail.com>
2023-06-04 22:52:09 +0530

    hive database

--- workout_tracker/lib/main.dart
+++ workout_tracker/lib/main.dart
@@ -1,9 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/adapters.dart';
 import 'package:provider/provider.dart';
 import 'package:workout_tracker/data/workout_data.dart';
 import 'pages/homepage.dart';
 
-void main() {
+void main() async {
+  //initialize hive
+  await Hive.initFlutter();
+
+
+  //open a hive box
+  await Hive.openBox("workout_database");
+
+
   runApp(const MyApp());
 }
 
--- workout_tracker/lib/pages/workout_page.dart
+++ workout_tracker/lib/pages/workout_page.dart
@@ -16,14 +16,86 @@ class _WorkoutPageState extends State<WorkoutPage> {
     Provider.of<WorkoutData>(context, listen: false)
         .checkOffExercise(workoutName, exerciseName);
   }
+//text controllers
+  final exerciseNameController = TextEditingController();
+  final weightController = TextEditingController();
+  final repsController = TextEditingController();
+  final setsController = TextEditingController();
+
 
   void createNewExercise(){
     showDialog(
         context: context,
-        builder: (context) => const AlertDialog(
+        builder: (context) => AlertDialog(
           title: Text('Add new exercise'),
+          content: SingleChildScrollView(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.center,
+
+              children: [
+                //exercise name
+                TextField(
+                  controller: exerciseNameController,
+                ),
+
+              //weight
+              TextField(
+                controller: weightController,
+              ),
+
+              //reps
+              TextField(
+                controller: repsController,
+              ),
+
+              //sets
+              TextField(
+                controller: setsController,
+              ),
+
+            ],),
+          ),
+
+          actions: [
+            MaterialButton(
+              onPressed: save,
+              child: const Text("save"),
+            ),
+            MaterialButton(
+              onPressed: cancel,
+              child: const Text("cancel"),
+            ),
+          ],
         ),
     );
+  }
+
+  void save() {
+    String newExerciseName = exerciseNameController.text;
+    String weight = weightController.text;
+    String reps = repsController.text;
+    String sets = setsController.text;
+
+    Provider.of<WorkoutData>(context, listen: false).addExercise(
+        widget.workoutName,
+        newExerciseName, weight, reps, sets);
+
+    Navigator.pop(context);
+    clear();
+  }
+
+  void cancel() {
+    Navigator.pop(context);
+    clear();
+  }
+
+  void clear() {
+    exerciseNameController.clear();
+    weightController.clear(); // Clear the text field
+    repsController.clear(); // Clear the text field
+    setsController.clear(); // Clear the text field
+// Clear the text field
   }
 
   @override
@@ -34,7 +106,7 @@ class _WorkoutPageState extends State<WorkoutPage> {
           title: Text(widget.workoutName),
         ),
         floatingActionButton: FloatingActionButton(
-          onPressed: () => createNewExercise,
+          onPressed: createNewExercise,
           child: Icon(Icons.add),
         ),
 
