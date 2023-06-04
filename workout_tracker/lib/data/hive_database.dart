e23b717195f0375ecb9997e78c3df429daa2560c
Dinethri Gunawardhana <dinethriupeksha200@gmail.com>
2023-06-04 23:31:04 +0530

    update

--- workout_tracker/lib/data/hive_database.dart
+++ workout_tracker/lib/data/hive_database.dart
@@ -1,1 +1,37 @@
-class HiveDatabase{}
+
+
+import '../models/workout.dart';
+
+class HiveDatabase{
+
+}
+
+List<String> convertObjectToWorkoutList(List<Workout>workouts){
+  List<String> workoutList = [
+
+  ];
+
+
+
+  for (int i=0; i<workouts.length; i++){
+    workoutList.add(
+      workouts[i].name,
+    );
+  }
+  return workoutList;
+}
+
+//convert exercises in a workout object into a list of strings
+
+List<List<List<String>>> convertObjectToExerciseList(List<Workout>workouts) {
+  List<List<List<String>>> exerciseList = [
+  ];
+
+
+//go through each workout
+  for (int i = 0; i < workouts.length; i++) {
+    //get exercise from each workout
+    List
+
+  }
+}
