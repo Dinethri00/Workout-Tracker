f92fd1d7b3a0f16fe07787aaf5ff99c78eb99513
Dinethri Gunawardhana <dinethriupeksha200@gmail.com>
2023-06-04 23:59:51 +0530

    UPDATE

--- workout_tracker/lib/data/hive_database.dart
+++ workout_tracker/lib/data/hive_database.dart
@@ -1,8 +1,24 @@
-
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:workout_tracker/datetime/date_time.dart';
+import 'package:workout_tracker/models/exercise.dart';
 
 import '../models/workout.dart';
 
 class HiveDatabase{
+
+  final _myBox = Hive.box("workout_database");
+
+  //check if already data stored, if not record the start date
+bool previousDataExists(){
+  if (_myBox.isEmpty){
+    print("Previous data does not exist");
+    _myBox.put("START DATE", todaysDateYYYYMMDD()); 
+    return false;
+  }else{
+    print("Previous data does exist");
+    return true;
+  }
+}
 
 }
 
@@ -31,7 +47,31 @@ List<List<List<String>>> convertObjectToExerciseList(List<Workout>workouts) {
 //go through each workout
   for (int i = 0; i < workouts.length; i++) {
     //get exercise from each workout
-    List
+    List<Exercise> exerciseInWorkout = workouts[i].exercises;
+
+    List<List<String>> individualWorkout = [
+
+    ];
+
+    for (int j=0; j<exerciseInWorkout.length; j++){
+      List<String> individualExercise = [
+
+      ];
+      individualExercise.addAll(
+        [
+          exerciseInWorkout[j].name,
+          exerciseInWorkout[j].weight,
+          exerciseInWorkout[j].reps,
+          exerciseInWorkout[j].sets,
+          exerciseInWorkout[j].isCompleted.toString(),
+
+
+        ],
+      );
+      individualWorkout.add(individualExercise);
+    }
+    exerciseList.add(individualWorkout);
 
   }
+  return exerciseList;
 }
