41e4b78bcf8875a66f3d78875c41e9e0f51773ab
Dinethri Gunawardhana <dinethriupeksha200@gmail.com>
2023-06-05 12:53:13 +0530

    heat map create

---
+++ workout_tracker/lib/components/heat_map.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_heatmap_calendar/flutter_heatmap_calendar.dart';
+import 'package:workout_tracker/datetime/date_time.dart';
+
+class MyHeatMap extends StatelessWidget {
+  final Map<DateTime, int>? datasets;
+  final String startDateYYYYMMDD;
+
+  const MyHeatMap({
+        super.key,
+        required this.datasets,
+        required this.startDateYYYYMMDD,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return  Container(
+      padding: EdgeInsets.all(25),
+      child: HeatMap(
+        startDate: createDateTimeObject(startDateYYYYMMDD),
+        endDate: DateTime.now().add(const Duration(days: 0)),
+        datasets: datasets,
+        colorMode: ColorMode.color,
+        defaultColor: Colors.grey[200],
+        textColor: Colors.white,
+        showColorTip: false,
+        showText: true,
+        scrollable: true,
+        size: 30,
+        colorsets: const {
+          1: Colors.green
+        },
+        onClick: (value) {
+          ScaffoldMessenger.of(context)
+              .showSnackBar(SnackBar(content: Text(value.toString())));
+        },
+      ),
+    );
+  }
+}
