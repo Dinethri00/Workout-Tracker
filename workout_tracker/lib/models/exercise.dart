1501602c7c79d3cf8e3d5e9d9a0e94452e3c5a1b
Dinethri Gunawardhana <dinethriupeksha200@gmail.com>
2023-06-13 22:57:15 +0530

    Update exercise.dart

--- workout_tracker/lib/models/exercise.dart
+++ workout_tracker/lib/models/exercise.dart
@@ -4,12 +4,12 @@ class Exercise {
   late final String reps;
   late final String sets;
   late bool isCompleted;
-//
-//   Exercise({
-//    required this.name,
-//    required this.weight,
-//    required this.reps,
-//    required this.sets,
-//    this.isCompleted = false,
-// });
-// }
+
+  Exercise({
+   required this.name,
+   required this.weight,
+   required this.reps,
+   required this.sets,
+   this.isCompleted = false,
+});
+}
